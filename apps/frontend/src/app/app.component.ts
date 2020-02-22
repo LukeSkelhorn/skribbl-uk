0d3c4f49300acf7c71e5980cbea801c9ccca3781
Luke Skelhorn <skelybones@Skels-MBP.local>
2020-02-22 15:57:02 +0000

    Added drawing canvas but needs updating waiting for the push

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import {
+  CanvasWhiteboardComponent,
+  CanvasWhiteboardOptions
+} from 'ng2-canvas-whiteboard';
+import { EventEmitter } from 'events';
 
 @Component({
   selector: 'skribbluk-root',
   templateUrl: './app.component.html',
+  viewProviders: [CanvasWhiteboardComponent],
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  @Output() canvasClear = new EventEmitter();
+  @Output() canvasUndo = new EventEmitter();
+  @Output() canvasRedo = new EventEmitter();
+  @Output() canvasSave = new EventEmitter();
+  canvasOptions: CanvasWhiteboardOptions = {
+    drawButtonEnabled: true,
+    drawButtonClass: 'drawButtonClass',
+    drawButtonText: 'Draw',
+    clearButtonEnabled: true,
+    clearButtonClass: 'clearButtonClass',
+    clearButtonText: 'Clear',
+    undoButtonText: 'Undo',
+    undoButtonEnabled: true,
+    redoButtonText: 'Redo',
+    redoButtonEnabled: true,
+    colorPickerEnabled: true,
+    saveDataButtonEnabled: true,
+    saveDataButtonText: 'Save',
+    lineWidth: 5,
+    strokeColor: 'rgb(0,0,0)',
+    shouldDownloadDrawing: true
+  };
+}
--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CanvasWhiteboardModule } from 'ng2-canvas-whiteboard';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, CanvasWhiteboardModule],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
+
+/**
+ * HelpFUl links
+ * https://github.com/webfactorymk/ng2-canvas-whiteboard
+ * https://github.com/tsand/responsive-sketchpad
+ * https://github.com/socketio/socket.io/blob/master/examples/whiteboard/public/main.js
+ */
